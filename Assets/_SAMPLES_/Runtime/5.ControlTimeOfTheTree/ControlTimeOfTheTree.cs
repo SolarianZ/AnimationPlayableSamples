9bbad55305a4e712582a2b957959c2f911403e4b
zqy <VDerGoW@hotmail.com>
2022-07-26 22:17:51 +0800

    5. ControlTimeOfTheTree

---
+++ Assets/_SAMPLES_/Runtime/5.ControlTimeOfTheTree/ControlTimeOfTheTree.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Animations;
+
+
+namespace GBG.AnimationPlayableSamples
+{
+    [RequireComponent(typeof(Animator))]
+    public class ControlTimeOfTheTree : MonoBehaviour
+    {
+        public AnimationClip clip;
+
+        public float time;
+
+        private PlayableGraph _graph;
+
+        private AnimationClipPlayable _clipPlayable;
+
+
+        private void Start()
+        {
+            _graph = PlayableGraph.Create("PlayableDemo-ControlTimeOfTheTree");
+
+            _clipPlayable = AnimationClipPlayable.Create(_graph, clip);
+
+            var animator = GetComponent<Animator>();
+            var output = AnimationPlayableOutput.Create(_graph, "AnimationOutput", animator);
+            output.SetSourcePlayable(_clipPlayable);
+
+            _graph.Play();
+
+            // Pause the clip playable, then we control the time manually
+            _clipPlayable.Pause();
+        }
+
+        private void Update()
+        {
+            _clipPlayable.SetTime(time);
+        }
+
+        private void OnDestroy()
+        {
+            _graph.Destroy();
+        }
+    }
+}
