507dc88f3c2db8f378a57ca9bb81ae1daa6bf556
zqy <VDerGoW@hotmail.com>
2022-07-26 22:17:11 +0800

    3. GraphWithMultiOutputs

---
+++ Assets/_SAMPLES_/Runtime/3.GraphWithMultiOutputs/GraphWithMultiOutputs.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Audio;
+using UnityEngine.Playables;
+
+namespace GBG.AnimationPlayableSamples
+{
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(AudioSource))]
+    public class GraphWithMultiOutputs : MonoBehaviour
+    {
+        public AnimationClip animationClip;
+
+        public AudioClip audioClip;
+
+        private PlayableGraph _graph;
+
+
+        private void Start()
+        {
+            _graph = PlayableGraph.Create("PlayableDemo-GraphWithMultiOutputs");
+
+            var animPlayable = AnimationClipPlayable.Create(_graph, animationClip);
+            var animator = GetComponent<Animator>();
+            var animOutput = AnimationPlayableOutput.Create(_graph, "AnimationOutput", animator);
+            animOutput.SetSourcePlayable(animPlayable);
+
+            var audioPlayable = AudioClipPlayable.Create(_graph, audioClip, true);
+            var audioSource = GetComponent<AudioSource>();
+            var audioOutput = AudioPlayableOutput.Create(_graph, "AudioOutput", audioSource);
+            audioOutput.SetSourcePlayable(audioPlayable);
+
+            _graph.Play();
+        }
+
+        private void OnDestroy()
+        {
+            _graph.Destroy();
+        }
+    }
+}
