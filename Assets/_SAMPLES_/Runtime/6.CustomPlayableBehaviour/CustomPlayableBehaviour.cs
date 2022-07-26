d03449ca38934df889bd3ce1e251e05cd2462434
zqy <VDerGoW@hotmail.com>
2022-07-26 22:18:06 +0800

    6. CustomPlayableBehaviour

---
+++ Assets/_SAMPLES_/Runtime/6.CustomPlayableBehaviour/CustomPlayableBehaviour.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+namespace GBG.AnimationPlayableSamples
+{
+    public class CustomPlayableBehaviour : PlayableBehaviour
+    {
+        private int _currentClipIndex = -1;
+
+        private float _timeToNextClip;
+
+        private Playable _mixer;
+
+
+        public void Initialize(AnimationClip[] clipsToPlay, Playable owner, PlayableGraph graph)
+        {
+            Debug.Log("#      CustomPlayableBehaviour::Initialize 0");
+
+            owner.SetInputCount(1);
+
+            _mixer = AnimationMixerPlayable.Create(graph, clipsToPlay.Length);
+
+            // mixer -> owner
+            graph.Connect(_mixer, 0, owner, 0);
+
+            owner.SetInputWeight(0, 1);
+
+            for (int i = 0; i < _mixer.GetInputCount(); i++)
+            {
+                var clipPlayable = AnimationClipPlayable.Create(graph, clipsToPlay[i]);
+                graph.Connect(clipPlayable, 0, _mixer, i);
+                _mixer.SetInputWeight(i, 1.0f);
+            }
+
+            Debug.Log("#      CustomPlayableBehaviour::Initialize 1");
+        }
+
+        public override void OnGraphStart(Playable playable)
+        {
+            Debug.Log("#      CustomPlayableBehaviour::OnGraphStart");
+        }
+
+        public override void OnGraphStop(Playable playable)
+        {
+            Debug.Log("#      CustomPlayableBehaviour::OnGraphStop");
+        }
+
+        public override void OnPlayableCreate(Playable playable)
+        {
+            Debug.Log("#      CustomPlayableBehaviour::OnPlayableCreate");
+        }
+
+        public override void OnPlayableDestroy(Playable playable)
+        {
+            Debug.Log("#      CustomPlayableBehaviour::OnPlayableDestroy");
+        }
+
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            Debug.Log("#      CustomPlayableBehaviour::OnBehaviourPlay");
+        }
+
+        public override void OnBehaviourPause(Playable playable, FrameData info)
+        {
+            Debug.Log("#      CustomPlayableBehaviour::OnBehaviourPause");
+        }
+
+        public override void PrepareData(Playable playable, FrameData info)
+        {
+            Debug.Log("#      CustomPlayableBehaviour::PrepareData");
+        }
+
+        public override void PrepareFrame(Playable playable, FrameData info)
+        {
+            Debug.Log("#      CustomPlayableBehaviour::PrepareFrame 0");
+
+            base.PrepareFrame(playable, info);
+
+            if (_mixer.GetInputCount() == 0)
+            {
+                return;
+            }
+
+            // Advance to next clip if necessary
+            _timeToNextClip -= (float)info.deltaTime;
+            if (_timeToNextClip <= 0f)
+            {
+                _currentClipIndex++;
+                if (_currentClipIndex >= _mixer.GetInputCount())
+                {
+                    _currentClipIndex = 0;
+                }
+
+                var currentClip = (AnimationClipPlayable)_mixer.GetInput(_currentClipIndex);
+                // Play from start
+                currentClip.SetTime(0);
+
+                //_timeToNextClip = currentClip.GetDuration();
+                _timeToNextClip = currentClip.GetAnimationClip().length;
+            }
+
+            // Adjust the weight of the inputs
+            for (int i = 0; i < _mixer.GetInputCount(); i++)
+            {
+                if (i == _currentClipIndex)
+                {
+                    _mixer.SetInputWeight(i, 1.0f);
+                }
+                else
+                {
+                    _mixer.SetInputWeight(i, 0.0f);
+                }
+            }
+
+            Debug.Log("#      CustomPlayableBehaviour::PrepareFrame 1");
+        }
+
+        public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+        {
+            Debug.Log("#      CustomPlayableBehaviour::ProcessFrame");
+        }
+
+    }
+}
---
+++ Assets/_SAMPLES_/Runtime/6.CustomPlayableBehaviour/CustomPlayableBehaviourComponent.cs
@@ -0,0 +1,99 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+namespace GBG.AnimationPlayableSamples
+{
+    [RequireComponent(typeof(Animator))]
+    public class CustomPlayableBehaviourComponent : MonoBehaviour
+    {
+        public AnimationClip[] clipsToPlay;
+
+        private PlayableGraph _graph;
+
+        private long _frame = 1;
+
+        private string _frameStr = null;
+
+
+        private string GetFrameStr()
+        {
+            if (string.IsNullOrEmpty(_frameStr))
+            {
+                _frameStr = _frame.ToString("D4");
+            }
+
+            return _frameStr;
+        }
+
+        private void Awake()
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        private void Start()
+        {
+            Debug.Log($"#{GetFrameStr()} CustomPlayableBehaviourComponent::Start 0", this);
+
+            _graph = PlayableGraph.Create("PlayableDemo-CustomPlayableBehaviour");
+
+            var playable = ScriptPlayable<CustomPlayableBehaviour>.Create(_graph);
+            var behaviour = playable.GetBehaviour();
+            behaviour.Initialize(clipsToPlay, playable, _graph);
+
+            var animator = GetComponent<Animator>();
+            var output = AnimationPlayableOutput.Create(_graph, "AnimationOutput", animator);
+            output.SetSourcePlayable(playable, 0);
+
+            _graph.Play();
+
+            Debug.Log($"#{GetFrameStr()} CustomPlayableBehaviourComponent::Start 1", this);
+        }
+
+        private void FixedUpdate()
+        {
+            Debug.Log($"#{GetFrameStr()} CustomPlayableBehaviourComponent::FixedUpdate", this);
+        }
+
+        private void Update()
+        {
+            Debug.Log($"#{GetFrameStr()} CustomPlayableBehaviourComponent::Update", this);
+        }
+
+
+        // Animation events
+        [SuppressMessage("CodeQuality", "IDE0051:删除未使用的私有成员", Justification = "<挂起>")]
+        private void OnFootstep()
+        {
+            Debug.Log($"#{GetFrameStr()} CustomPlayableBehaviourComponent::OnFootstep(animation events)", this);
+        }
+
+        private void OnAnimatorMove()
+        {
+            Debug.Log($"#{GetFrameStr()} CustomPlayableBehaviourComponent::OnAnimatorMove", this);
+        }
+
+        private void OnAnimatorIK(int layerIndex)
+        {
+            Debug.Log($"#{GetFrameStr()} CustomPlayableBehaviourComponent::OnAnimatorIK", this);
+        }
+
+        private void LateUpdate()
+        {
+            Debug.Log($"#{GetFrameStr()} CustomPlayableBehaviourComponent::LateUpdate", this);
+
+            _frame++;
+            _frameStr = null;
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log($"#{GetFrameStr()} CustomPlayableBehaviourComponent::OnDestroy 0", this);
+
+            _graph.Destroy();
+
+            Debug.Log($"#{GetFrameStr()} CustomPlayableBehaviourComponent::OnDestroy 1", this);
+        }
+    }
+}
